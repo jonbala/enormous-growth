9c8a091e75885544def437de8eecb015d48f5854
jonbala <jonijoni929@gmail.com>
2023-04-05 15:21:11 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import connectDB from "./mongodb/connect.js";
+import userRouter from "./routes/user.routes.js";
+import postRouter from "./routes/post.routes.js";
+import { users, posts } from "./data/index.js";
+import User from "./mongodb/models/user.js";
+import Post from "./mongodb/models/post.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+
+    app.listen(8080, () =>
+      console.log("Server started on port http://localhost:8080")
+    );
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
